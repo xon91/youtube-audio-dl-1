b858812e48375a35cd8c2b725ff64f7d1213c5b6
Kevin Zúñiga <kevin.zun@gmail.com>
2017-05-29 00:32:17 -0500

    Add webpack and babel config

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+/* global __dirname, require, module */
+
+const webpack = require('webpack');
+const path = require('path');
+
+const libraryName = 'youtube-audio-dl';
+
+const config = {
+  entry: path.resolve(__dirname, 'src/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    library: libraryName,
+    libraryTarget: 'umd',
+    umdNamedDefine: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    modules: [path.resolve(__dirname, 'src')],
+    extensions: ['.js', '.json']
+  }
+};
+
+module.exports = config;
