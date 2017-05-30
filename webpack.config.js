52b0d3e2d958cb95e0155f5d25c4a6c6dafd11c2
Kevin Zúñiga <kevin.zun@gmail.com>
2017-05-30 18:18:31 -0500

    Add ESLint

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,12 @@ const config = {
   module: {
     rules: [
       {
+        enforce: "pre",
+        test: /\.js$/,
+        loader: "eslint-loader",
+        exclude: /node_modules/
+      },
+      {
         test: /\.js$/,
         loader: 'babel-loader',
         exclude: /node_modules/
