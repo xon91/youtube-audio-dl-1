06d1e824f06e6b16aa91c1f1aa788abaacc30f89
Kevin Zúñiga <kevin.zun@gmail.com>
2017-05-31 22:00:09 -0500

    Add jest

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+const config = {
+  'moduleFileExtensions': ['js', 'json'],
+  'moduleDirectories': [
+    '<rootDir>/src',
+    'node_modules'
+  ],
+  'testEnvironment': 'node'
+};
+
+module.exports = config;
