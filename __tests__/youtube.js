3612a1eb33a2571f58e500837751fa5f156a6239
Kevin Zúñiga <kevin.zun@gmail.com>
2017-06-24 01:19:44 -0500

    Add tests to get video id

--- __tests__/youtube.js
+++ __tests__/youtube.js
@@ -2,6 +2,7 @@
 /* global test, expect */
 import axios from 'axios';
 import HTMLParser from 'fast-html-parser';
+import YouTubeIE from 'YouTubeIE';
 
 test('get youtube video title',  () => {
   const promise = axios.get('https://www.youtube.com/watch?v=NdYWuo9OFAw', {
@@ -22,4 +23,16 @@ test('get youtube video title',  () => {
     });
 
   return expect(promise).resolves.toMatch(/Iris/);
+});
+
+test('get youtube id from url', () => {
+  const ie = new YouTubeIE;
+  const id = ie.getVideoId('https://www.youtube.com/watch?v=NdYWuo9OFAw');
+  return expect(id).toBe('NdYWuo9OFAw');
+});
+
+test('bad youtube url', () => {
+  const ie = new YouTubeIE;
+  const id = ie.getVideoId('https://www.notyoutube.com/watch?v=NdYWuo9OFAw');
+  return expect(id).toBeNull();
 });
